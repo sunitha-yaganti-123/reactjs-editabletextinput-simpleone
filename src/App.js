4c8008779fbeb36794c07b14266843ca778a888e
sunitha_yaganti <yvvlsunitha@gmail.com>
2022-08-18 07:47:10 +0000

    message

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import AppItem from './components/AppItem'
+
+import './App.css'
+
+const App = () => <AppItem />
+
+export default App
---
+++ src/components/AppItem/index.js
@@ -0,0 +1,54 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class AppItem extends Component {
+  state = {isClicked: false, ent: ''}
+
+  one = event => {
+    this.setState({ent: event.target.value})
+  }
+
+  start = () => {
+    this.setState(prevState => ({isClicked: !prevState.isClicked}))
+  }
+
+  render() {
+    const {isClicked, ent} = this.state
+    return (
+      <div className="container-one">
+        <div className="container-two">
+          <h1 className="header">Editable Text Input</h1>
+          <div className="container-three">
+            {isClicked === true ? (
+              <div className="paragraph-icon">
+                <p className="p">{ent}</p>
+                <button type="button" className="but" onClick={this.start}>
+                  Edit
+                </button>
+              </div>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  value={ent}
+                  className="inp"
+                  onChange={this.one}
+                />
+                <button
+                  className="button-edit"
+                  type="button"
+                  onClick={this.start}
+                >
+                  Save
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AppItem
